cb0c57a9223126d725eb85596250ccb0f33a3720
ttimbal <timbal.tmb@gmail.com>
2024-05-07 22:41:41 -0400

    Add migrations

---
+++ database/migrations/2024_05_08_020415_create_pedido_producto_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pedido_producto', function (Blueprint $table) {
+            $table->id();
+            $table->integer('cantidad');
+            $table->float('precio_unitario');
+
+            $table->unsignedBigInteger('producto_id');
+            $table->unsignedBigInteger('pedido_id');
+            $table->foreign('producto_id')->references('id')->on('productos')->onDelete('cascade');
+            $table->foreign('pedido_id')->references('id')->on('pedidos')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pedido_producto');
+    }
+};
---
+++ database/migrations/2024_05_08_020425_create_facturas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('facturas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nit', 15);
+            $table->string('nombre', 50);
+            $table->dateTime('fecha_emision');
+
+            $table->unsignedBigInteger('pedido_id');
+            $table->foreign('pedido_id')->references('id')->on('pedidos')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('facturas');
+    }
+};
