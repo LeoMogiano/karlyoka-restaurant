cbd282f760f9d2594a3fe5bb62be09c4a6b8c293
Leo Mogiano <leomogiano@outlook.com>
2024-05-08 12:13:48 -0400

    Fix UserSeeder instance

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+        $user = new User([
+            'name' => 'Test',
+            'apellidos' => 'User',
+            'rol' => 'cajero', // TODO: Reminder dueño y cajero
+            'email' => 'test@example.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
+        $user->save();
+    }
+}
